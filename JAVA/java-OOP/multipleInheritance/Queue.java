861b651eade5b18146e17a751955ad2b89a1fa99
hyunsb <96504592+hyunsb@users.noreply.github.com>
2023-03-29 21:48:49 +0900

    230329 - Java: 인터페이스 다중 상속

---
+++ JAVA/java-OOP/multipleInheritance/Queue.java
@@ -0,0 +1,9 @@
+package multipleInheritance;
+
+public interface Queue {
+
+    void enQueue(String title);
+    String deQueue();
+
+    int getSize();
+}
---
+++ JAVA/java-OOP/multipleInheritance/Shelf.java
@@ -0,0 +1,20 @@
+package multipleInheritance;
+
+import java.util.ArrayList;
+
+public class Shelf {
+
+    protected ArrayList<String> shelf;
+
+    public Shelf() {
+        shelf = new ArrayList<>();
+    }
+
+    public ArrayList<String> getShelf() {
+        return shelf;
+    }
+
+    public int getCount(){
+        return shelf.size();
+    }
+}
